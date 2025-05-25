9302ea53c73165e8c74ade7b1482312967a1b178
Karassvo <168686462+Karassvo@users.noreply.github.com>
2025-05-25 09:36:38 +0500

    fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import './App.css';
-import GameContainer from './container/GameContainer';
+// src/App.tsx
+
+// import './App.css'; // Эту строку мы удалили
+
+import GameContainer from './container/GameContainer'; // ИСПРАВЛЕННЫЙ ПУТЬ
 
 function App() {
   return (
